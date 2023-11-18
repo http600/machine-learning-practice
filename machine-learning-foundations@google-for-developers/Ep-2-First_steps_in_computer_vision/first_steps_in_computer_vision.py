e55e9fa446b8505f9381dfc01f3531f22ffe08f8
http418 <http418@163.com>
2023-11-18 22:09:43 +0800

    simple

--- machine-learning-foundations@google-for-developers/Ep-2-First_steps_in_computer_vision/first_steps_in_computer_vision.py
+++ machine-learning-foundations@google-for-developers/Ep-2-First_steps_in_computer_vision/first_steps_in_computer_vision.py
@@ -1,10 +1,7 @@
-
-# from tensorflow import keras
-import keras
 import tensorflow as tf
 
 
-class Inject(keras.callbacks.Callback):
+class Inject(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
         if (logs.get('accuracy') > 0.99):
             print('\n 99%% accuracy stop now')
@@ -12,16 +9,17 @@ class Inject(keras.callbacks.Callback):
 
 
 def run():
-    fasion_mnist = keras.datasets.fashion_mnist
+    fasion_mnist = tf.keras.datasets.mnist
     (X, Y), (x_check, y_check) = fasion_mnist.load_data()
     X, x_check = X / 255.0, x_check / 255.0
-    model = keras.Sequential([keras.layers.Flatten(input_shape=(28, 28)),
-                              keras.layers.Dense(128, activation=tf.nn.relu),
-                              keras.layers.Dense(10, activation=tf.nn.softmax)])
-    model.compile(optimizer=keras.optimizers.legacy.Adam(),
-                  loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
-    model.fit(X, Y, epochs=50, callbacks=[Inject()])
-    print(model.evaluate(x_check, y_check))
+    model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
+                                 tf.keras.layers.Dense(
+                                     512, activation=tf.nn.relu),
+                                 tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+    model.compile(optimizer=tf.keras.optimizers.legacy.Adam(),
+                  loss=tf.keras.losses.sparse_categorical_crossentropy,
+                  metrics=['accuracy'])
+    model.fit(X, Y, epochs=10, callbacks=[Inject()])
 
 
 if __name__ == '__main__':
